b5594b53194b47681f56a29380cb25cb64e69c56
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-22 15:41:41 +0600

    This is my first problem solution using python. the problem is taking input of two number from user and calculating their summation

---
+++ sum-of-two-number.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the first number here: "))
+num2 = int(input("Enter the second number here: "))
+sum_of_two_number = num1 + num2
+print("Sum of two number is: ", sum_of_two_number)
