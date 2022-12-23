23b152c2259c5d029fd79a1ef2e697bc9b56ede1
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-23 16:40:20 +0600

    Tried to make a very simple dictionary and access the through user input.

---
+++ making-a-simple-dictionay.py
@@ -0,0 +1,14 @@
+my_dict = {
+    "advocate": "to publicly support or suggest an idea, development, or way of doing something",
+    "leverage": "the action or advantage of using a lever",
+    "content": "pleased with your situation and not hoping for change or improvement",
+    "concern": "to cause worry to someone",
+    "sentence": "a group of words, usually containing a verb, that expresses a thought in the form of a statement, "
+                "question, instruction, or exclamation and starts with a capital letter when written",
+
+}
+
+word = input("Enter the word here: ")
+print(word, ":", my_dict[word])
+
+
