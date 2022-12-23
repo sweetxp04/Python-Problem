e680f3a51e876a5466ce60a474046b7eaff3d116
Mahidur Rahman <mahidurrahman.eee@gmail.com>
2022-12-23 18:20:59 +0600

    Solved 4 basic problems

---
+++ perimeter-of-triangle.py
@@ -0,0 +1,5 @@
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter = a+b+c
+print("Perimeter of the triangle is:",perimeter)
---
+++ radius-of-circle.py
@@ -0,0 +1,5 @@
+redius = int(input("Enter radius: "))
+area = 3.1416*redius*redius
+circumference = 2*3.1416*redius
+print("Area of the circle:", area)
+print("Circumference of the circle", circumference)
