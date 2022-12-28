1fb3bb0a555d83ed9ec50ed54936c647ed62df46
sweetxp04 <mahidurrahman.eee@gmail.com>
2022-12-28 23:47:53 +0600

    Modified the code

--- Area-of-a-triangle.py
+++ Area-of-a-triangle.py
@@ -1,4 +1,4 @@
 height = int(input("Enter height of the triangle: "))
 base = int(input("Enter base of the triangle: "))
 area = 0.5*height*base
-print("Area of the triangle:",area)
+print("Triangle Area:",area)
